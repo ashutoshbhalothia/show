303ec68856d63e3c67b1a8c192e4ae30ad745599
ashutoshbhalothia <ashutoshbhalothia@gmail.com>
2024-02-14 13:09:03 +0530

    logger added

--- src/main/java/com/backend/show/service/BaseMongoService.java
+++ src/main/java/com/backend/show/service/BaseMongoService.java
@@ -1,5 +1,7 @@
 package com.backend.show.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Component;
@@ -9,6 +11,8 @@ import java.util.List;
 
 @Component
 public class BaseMongoService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(BaseMongoService.class);
     @Autowired
     private MongoTemplate mongoTemplate;
 
@@ -20,6 +24,7 @@ public class BaseMongoService {
         try {
             return mongoTemplate.findAll(clazz,collectionName);
         }catch (Exception e){
+            LOGGER.error("Error encountered while fetching data from DB : {}",e.getMessage());
             return Collections.emptyList();
         }
     }
